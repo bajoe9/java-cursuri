349c5440f9035862ec2c75651d0f716d791e1f77
andreimarinescu <marinescu995>
2018-11-09 18:04:31 +0200

    no new message but link port

---
+++ src/data.java
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+public class data {
+
+    public static void main(String[] args) {
+        int myAge;
+        myAge=23;
+        float weight = 90f;
+        double height = 180;
+        long distance = 8987878787136545632l;
+        char gender = 'm';
+        char test = 9;
+        boolean isStudent = false;
+        boolean hasJob = true;
+        int test2 = (int) weight;
+        System.out.print(test);
+        System.out.print(test2);
+
+
+
+    }}
