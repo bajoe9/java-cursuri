48128ba12ea5e8583503dca073268c0d8932795f
bajoe9 <44846502+bajoe9@users.noreply.github.com>
2019-01-08 20:55:16 +0200

    Add files via upload

---
+++ java_advance/src/main/java/Savarina.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Data
+
+public class Savarina {
+    private String culoare;
+    private String gust;
+
+}
---
+++ java_advance/src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Savarina s = new Savarina("alb", "dulce");
+
+
+        System.out.println("Mananc o savarina " + s.getCuloare() + " si " +s.getGust());
+
+    }
+}
